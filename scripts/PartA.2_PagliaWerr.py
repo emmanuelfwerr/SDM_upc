02940dfd33ea968df7f15122afd025549ad8c3f3
Emmanuel F. Werr <47487994+emmanuelfwerr@users.noreply.github.com>
2023-03-15 23:45:32 +0100

    final update PartA.2

--- scripts/PartA.2_PagliaWerr.py
+++ scripts/PartA.2_PagliaWerr.py
@@ -16,83 +16,76 @@ URI = 'neo4j://localhost:7687'
 AUTH = ('neo4j', 'password')
 DB_NAME = 'neo4j'
 
+def cypher_query_write(query, driver):
+    '''
+
+    '''
+    try:
+        with driver.session(database=DB_NAME) as session:
+            response = list(session.run(query))
+        print('Transaction Completed Succesfully!')
+        return response
+    except Exception as e:
+        print("Query failed: ", e)
+
 
 def main():
     '''
     
     '''
-    # 
+    # initialize neo4j driver
     with GraphDatabase.driver(URI, auth=AUTH) as driver:
-        #
+        # run query transactions to create all nodes
         for query in cypher_init_nodes:
-            try:
-                with driver.session(database=DB_NAME) as session:
-                    response = list(session.run(query))
-                    print(response)
-            except Exception as e:
-                print("Query failed: ", e)
+            cypher_query_write(query, driver)
 
+        # run query transactions to create all relationships
         for query in cypher_init_relations:
-            try:
-                with driver.session(database=DB_NAME) as session:
-                    response = list(session.run(query))
-                    print(response)
-            except Exception as e:
-                print("Query failed: ", e)
+            cypher_query_write(query, driver)
 
 
 # A.2 - load graph nodes from CSV
 cypher_init_nodes = [
     ''' LOAD CSV WITH HEADERS FROM 'file:///papers.csv' AS row FIELDTERMINATOR ';' 
-        CREATE (:Paper {ID: row.id, Title: row.title, Abstract: row.abstract});''',
+        CREATE (:Paper {ID: row.paper_id, Title: row.title, Abstract: row.abstract});''',
 
     ''' LOAD CSV WITH HEADERS FROM 'file:///authors.csv' AS row FIELDTERMINATOR ';' 
-        CREATE (:Person {ID: row.id, Name: row.author});''',
+        CREATE (:Person {ID: row.author_id, Name: row.author_name});''',
 
     ''' LOAD CSV WITH HEADERS FROM 'file:///keywords.csv' AS row FIELDTERMINATOR ';' 
-    CREATE (:Keyword {ID: row.keyword_id, Keyword: row.keyword_name});''',
+        CREATE (:Keyword {ID: row.keyword_id, Keyword: row.keyword_name});''',
 
     ''' LOAD CSV WITH HEADERS FROM 'file:///topics.csv' AS row FIELDTERMINATOR ';'
         CREATE (:Topic {ID: row.topic_id, Topic: row.topic_name});''',
 
     ''' LOAD CSV WITH HEADERS FROM 'file:///conferences.csv' AS row FIELDTERMINATOR ';' 
-        CREATE (:Conference {ID: row.publisher_id, Conference: row.conference_name});''',
+        CREATE (:Conference {ID: row.conference_id, Conference: row.conference_name});''',
 
-    ''' LOAD CSV WITH HEADERS FROM 'file:///conferences.csv' AS row FIELDTERMINATOR ';' 
-        CREATE (:Edition {ID: row.publisherType_id, Edition: row.edition, City: row.venue, Year: row.year, Season: row.season});''',
-
-    ''' LOAD CSV WITH HEADERS FROM 'file:///journals.csv' AS row FIELDTERMINATOR ';' 
-        CREATE (:Journal {ID: row.publisher_id, Journal: row.journal_name});''',
+    ''' LOAD CSV WITH HEADERS FROM 'file:///editions.csv' AS row FIELDTERMINATOR ';' 
+        CREATE (:Edition {ID: row.edition_id, Edition: row.edition, City: row.venue, Year: row.year, Season: row.season});''',
 
     ''' LOAD CSV WITH HEADERS FROM 'file:///journals.csv' AS row FIELDTERMINATOR ';' 
-        CREATE (:Volume {ID: row.publisherType_id, Volume: row.volume, Year: row.year});'''
+        CREATE (:Journal {ID: row.journal_id, Journal: row.journal_name});''',
+
+    ''' LOAD CSV WITH HEADERS FROM 'file:///volumes.csv' AS row FIELDTERMINATOR ';' 
+        CREATE (:Volume {ID: row.volume_id, Volume: row.volume, Year: row.year});'''
 ]
 
 # A.2 - load graph edges from CSV
 cypher_init_relations = [
-    ''' LOAD CSV WITH HEADERS FROM "file:///paper_TO_person.csv" AS row FIELDTERMINATOR ';' 
+    ''' LOAD CSV WITH HEADERS FROM "file:///paper_TO_author.csv" AS row FIELDTERMINATOR ';' 
         MERGE (paper:Paper {ID: row.paper_id})
         MERGE (person:Person {ID: row.author_id})
         MERGE (paper)-[:WritenBy]->(person);''',
 
-    ''' LOAD CSV WITH HEADERS FROM "file:///paper_TO_person.csv" AS row FIELDTERMINATOR ';' 
+    ''' LOAD CSV WITH HEADERS FROM "file:///paper_TO_author.csv" AS row FIELDTERMINATOR ';' 
         MERGE (paper:Paper {ID: row.paper_id})
         MERGE (person:Person {ID: row.co_author_id})
         MERGE (paper)-[:CoauthoredBy]->(person);''',
 
-    ''' LOAD CSV WITH HEADERS FROM "file:///paper_TO_person.csv" AS row FIELDTERMINATOR ';' 
+    ''' LOAD CSV WITH HEADERS FROM "file:///paper_TO_reviewer.csv" AS row FIELDTERMINATOR ';' 
         MERGE (paper:Paper {ID: row.paper_id})
-        MERGE (person:Person {ID: row.reviewer1_id})
-        MERGE (paper)-[:ReviewedBy]->(person);''',
-
-    ''' LOAD CSV WITH HEADERS FROM "file:///paper_TO_person.csv" AS row FIELDTERMINATOR ';' 
-        MERGE (paper:Paper {ID: row.paper_id})
-        MERGE (person:Person {ID: row.reviewer2_id})
-        MERGE (paper)-[:ReviewedBy]->(person);''',
-
-    ''' LOAD CSV WITH HEADERS FROM "file:///paper_TO_person.csv" AS row FIELDTERMINATOR ';' 
-        MERGE (paper:Paper {ID: row.paper_id})
-        MERGE (person:Person {ID: row.reviewer3_id})
+        MERGE (person:Person {ID: row.reviewer_id})
         MERGE (paper)-[:ReviewedBy]->(person);''',
 
     '''LOAD CSV WITH HEADERS FROM "file:///paper_TO_paper.csv" AS row FIELDTERMINATOR ';' 
